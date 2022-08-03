865dd8a3e8c43322c9e020c26be5a3fec27c8a7c
神代泰宏 <yasuhiro79367s@hotmail.co.jp>
2022-08-03 09:42:03 +0900

    add register view presentation

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -9,9 +9,14 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.myapplication.ui.theme.MyApplicationTheme
 import java.text.SimpleDateFormat
 import java.util.*
@@ -23,7 +28,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyApplicationTheme {
-                // A surface container using the 'background' color from the theme
+                val navController = rememberNavController()
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
@@ -35,7 +40,14 @@ class MainActivity : ComponentActivity() {
                     aaa.date = Date()
                     list.add(aaa)
 
-                    HomeContent(taskList = list)
+                    NavHost(navController = navController, startDestination = "main") {
+                        composable("main") {
+                            HomeContent(taskList = list, navController = navController)
+                        }
+                        composable("second") {
+                            Register()
+                        }
+                    }
                 }
             }
         }
@@ -43,7 +55,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun HomeContent(taskList: List<Task>) {
+fun HomeContent(taskList: List<Task>, navController: NavController) {
     Scaffold(
         topBar = {
             TopAppBar(title = { Text("タスク一覧") })
@@ -51,7 +63,7 @@ fun HomeContent(taskList: List<Task>) {
         floatingActionButton = {
             FloatingActionButton(
                 onClick = {
-                          /*TODO*/
+                          navController.navigate("second")
                           },
                 content = {
                     Icon(
@@ -80,6 +92,27 @@ private fun TaskList(taskList: List<Task>){
     Divider()
 }
 
+@Composable
+private fun Register() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("タスク登録・編集") }
+            )
+        }
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Text(
+                text = "詳細画面"
+            )
+        }
+    }
+}
+
 
 @Composable
 @Preview
@@ -89,13 +122,21 @@ fun DefaultPreview() {
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colors.background
         ) {
+            val navController = rememberNavController()
             val list = mutableListOf<Task>()
             val aaa = Task()
             aaa.id = 1L
             aaa.title = "memomemomemo"
             list.add(aaa)
 
-            HomeContent(list)
+            NavHost(navController = navController, startDestination = "main") {
+                composable("main") {
+                    HomeContent(taskList = list, navController = navController)
+                }
+                composable("second") {
+                    Register()
+                }
+            }
         }
     }
 }
